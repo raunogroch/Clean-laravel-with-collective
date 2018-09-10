3b7d48a82d26ea50f7a0bffeeb9797cc06099df2
Raul Ariel Noguera Rocha <raul.noguera.rocha@gmail.com>
2018-09-10 00:04:21 -0400

    changing message from welcome file by uppercase ranr

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Updated by ranr</div>
+                <div class="title">Updated by R.A.N.R.</div>
             </div>
         </div>
     </body>
